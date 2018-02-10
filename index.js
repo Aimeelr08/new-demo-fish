1641188937dfafc26a398422c0258d7a752b05f9
EC2 Default User <ec2-user@ip-172-31-12-24.us-east-2.compute.internal>
2018-02-10 22:15:35 +0000

    added in index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
- require("./src/index.js");
+
+const request = function () {
+const apiBaseUrl = require("https://fishbase.ropensci.org/seabase");
+    console.log('get api');
+}
+'use strict';
+
+// const app = {
+//   // host: 'http://localhost:/'
+// host: 'https://fishbase.ropensci.org/seabase'
+// };
+
+// module.exports = app;
